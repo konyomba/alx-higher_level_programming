2836d2a8b4ebbd2b62844f6f6ebb023782aa497b
konyomba <konyomba@gmail.com>
2024-01-07 01:59:40 -0800

    task8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if not sentence:
+        return None
+    if sentence:
+        sentence_length = len(sentence)
+    else:
+        sentence_length = o
+    return (sentence_length, sentence if not sentence else sentence[:1])
