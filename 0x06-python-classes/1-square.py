8be0e3353e722b33f1334e1c9ca65e5144a35ce2
konyomba <konyomba@gmail.com>
2024-01-23 11:38:01 -0800

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define a class Square"""
+
+
+class Square:
+    """Represent square"""
+
+    def __init__(self, size):
+        """Initialize new Square
+
+        Args:
+            size (int): size of new square"""
+        self.__size = size
